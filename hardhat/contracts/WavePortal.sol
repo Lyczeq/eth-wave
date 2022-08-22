d99b86a53c68c35a1026d3cd7c3e9918cf39c05a
Lyczeq <lyczeq@gmail.com>
2022-08-22 21:45:43 +0200

    Created a smart contract with the script and tests included

---
+++ hardhat/contracts/WavePortal.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract WavePortal {
+    struct Wave {
+        address userAddress;
+        string message;
+        uint256 timestamp;
+    }
+
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    Wave[] waves;
+
+    mapping(address => uint256) public lastWavedAt;
+
+    function wave(string memory _message) public {
+        require(
+            lastWavedAt[msg.sender] + 15 minutes < block.timestamp,
+            "Wait 15 minutes"
+        );
+
+        lastWavedAt[msg.sender] = block.timestamp;
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getWaveMessages() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        return waves.length;
+    }
+}
--- hardhat/scripts/run.js
+++ hardhat/scripts/run.js
@@ -1,9 +1,28 @@
 const main = async () => {
   // compiles the contract and prepares the files to work with it under the artifacts directory
   const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
+
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+
+  // Hardhat creates a local Ethereum network only for that contract. When the script is finished, the network is destroyed. Every time the contract is rum there is a fresh blockchain.
   const waveContract = await waveContractFactory.deploy();
+
+  // wait until contract is deployed
   await waveContract.deployed();
   console.log('Contract deployed to:', waveContract.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  let waveTxn = await waveContract.wave('test');
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson).wave("test2");
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
